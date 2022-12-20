5207a8aebdeaae9d6a8f57141bc7f1185127c624
Mark Essen <essen93@bk.ru>
2022-12-20 12:25:08 +0400

    Добавил поля классу Database

--- LabApp/Utility/Database.cs
+++ LabApp/Utility/Database.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WindowsFormsApp1.Utility
 {
-    internal class Database
+    public static class Database
     {
+        static string connectionString = "server=127.0.0.1;uid=root;pwd=;database=lab_db";
+        public static MySqlConnection connection = new MySqlConnection(connectionString);
     }
 }
